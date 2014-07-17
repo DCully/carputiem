0329f8376e2bb5afc77e251a7f9c281a763f2fcd
David Cully <david.a.cully@gmail.com>
2014-07-17 17:35:40 -0400

    forgot to add these files

---
+++ LCDinterface/mockIOHandler.cpp
@@ -0,0 +1,63 @@
+#include "mockIOHandler.h"
+#include <iostream>
+#include <thread>
+#include "Controller.h"
+
+// spits things out to cerr  and allows console input to sub for buttons (for testing)
+
+using std::cin;
+using std::cerr;
+using std::endl;
+
+IOHandlerInterface::IOHandlerInterface()
+{
+    std::thread(&IOHandlerInterface::testThread, this).detach();
+}
+
+void IOHandlerInterface::moveCursor(const int& spot)
+{
+    cerr << "CURSOR moved by an LSB to " << spot << endl;
+}
+
+// prints static text to LCD
+void IOHandlerInterface::printToLCD(const std::string& text, const int& spot)
+{
+    cerr << "PRINT: '" << text << "' (" << text.size() << " chars) at " << spot << endl;
+}
+
+// starts up to 3 scrolling lines
+void IOHandlerInterface::startScrollText(const std::vector<size_t>& startSpots,
+                                         const std::vector<size_t>& stopSpots,
+                                         const std::vector<size_t>& lineNums,
+                                         const std::vector<std::string>& msg)
+{
+    cerr << "STARTSCROLLTEXT called: " << endl;
+    for (size_t x = 0; x < lineNums.size(); ++x) {
+        cerr << "  Scroll #" << x << ":" << endl;
+        cerr << "    from " << startSpots.at(x) << " to " << stopSpots.at(x) << " on line " << lineNums.at(x) << endl;
+        cerr << "    text: " << msg.at(x) << " (" << msg.at(x).size() << " chars) " << endl;
+    }
+}
+
+// flips scrolling thread to paused
+void IOHandlerInterface::stopAllScrollingText()
+{
+    cerr << "STOPSCROLL called" << endl;
+}
+
+void IOHandlerInterface::testThread()
+{
+    char input;
+    while (true) {
+        cin >> input;
+        if (input == 'L') {
+            Controller::staticLeftButPressed();
+        }
+        else if (input == 'R') {
+            Controller::staticRightButPressed();
+        }
+        else if (input == 'S') {
+            Controller::staticSelectPressed();
+        }
+    }
+}
---
+++ LCDinterface/mockIOHandler.h
@@ -0,0 +1,37 @@
+#ifndef MOCKIOHANDLER_H
+#define MOCKIOHANDLER_H
+
+#include <string>
+#include <vector>
+
+/* This is the generalized IOHandler interface
+*  All these virtual functions are overriden by the IOHandler class
+*  This parent class prints everything to console instead of a character LCD
+*  This way, we can test everything but new IOHandler implementations without a Pi
+*  Attach keyboard inputs to simulate GPIO button presses?
+*/
+class IOHandlerInterface
+{
+    public:
+        IOHandlerInterface();
+        virtual ~IOHandlerInterface() {}
+
+        // changes user's cursor position
+        virtual void moveCursor(const int& spot);
+
+        // prints static text to LCD
+        virtual void printToLCD(const std::string& text, const int& spot);
+
+        // starts up to 3 scrolling lines
+        virtual void startScrollText(const std::vector<size_t>& startSpots,
+            const std::vector<size_t>& stopSpots,
+            const std::vector<size_t>& lineNums,
+            const std::vector<std::string>& msg);
+
+        // flips scrolling thread to paused
+        virtual void stopAllScrollingText();
+    private:
+        void testThread();
+};
+
+#endif
